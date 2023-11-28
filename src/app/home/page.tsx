644a5d14ffa842263ad4b183cd3a4d95c0e933c0
mubin_mhd <ariamubin79@gmail.com>
2023-11-28 15:46:32 +0700

    add home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,5 +2,137 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-gradient-to-r from-blue-600 to-indigo-500">
+      <div className="mx-auto h-full px-4 py-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-16 xl:py-28">
+        <div className="flex flex-col items-center justify-between">
+          <div className="">
+            <div className="max-w-3xl">
+              <h1 className="mb-6 text-center text-5xl font-light tracking-tight text-white sm:text-5xl lg:text-7xl">
+                Easily Shop{" "}
+                <span className="my-1 inline-block font-serif font-bold text-white">
+                  {" "}
+                  wait while sleeping{" "}
+                </span>
+              </h1>
+              <p className="text-center text-base text-gray-100">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque it.
+              </p>
+            </div>
+            <form
+              method="POST"
+              action="#"
+              className="mx-auto mt-12 mb-2 max-w-xl sm:rounded-xl sm:border sm:border-gray-100 sm:bg-white sm:p-2 sm:shadow"
+            >
+              <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
+                <div className="relative text-gray-500 sm:w-7/12">
+                  <label
+                    htmlFor="email"
+                    className="sr-only border-gray-300"
+                  ></label>
+                  <div className="pointer-events-none absolute mr-2 inset-y-0 left-0 flex items-center pl-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="28"
+                      height="28"
+                      fill="#6e6e81"
+                      viewBox="0 0 256 256"
+                    >
+                      <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,160H40V56H216V200ZM176,88a48,48,0,0,1-96,0,8,8,0,0,1,16,0,32,32,0,0,0,64,0,8,8,0,0,1,16,0Z"></path>
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="What do you want to buy?"
+                    className="w-full cursor-text rounded-xl border-2 py-4 pr-4 pl-10 text-base outline-none transition-all duration-200 ease-in-out sm:border-0 focus:border-transparent focus:ring"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="group flex items-center justify-center rounded-xl bg-blue-700 px-6 py-4 text-white transition"
+                >
+                  <span className="group flex w-full items-center justify-center rounded text-center font-medium">
+                    Shop Now
+                  </span>
+                  <svg
+                    className="shrink-0 group-hover:ml-8 ml-4 h-4 w-4 transition-all"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </form>
+
+            <div className="divide-gray-300/30 mt-12 flex flex-col items-center justify-center space-y-3 text-sm text-gray-700 sm:flex-row sm:items-start sm:space-y-0 sm:divide-x">
+              <div className="flex max-w-xs space-x-2 px-4">
+                <span className="shrink-0 flex h-14 w-14 items-center justify-center rounded-xl bg-white p-2.5 text-purple-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28"
+                    height="28"
+                    fill="#6e6e81"
+                    viewBox="0 0 256 256"
+                  >
+                    <path d="M224,40V80a8,8,0,0,1-16,0V48H176a8,8,0,0,1,0-16h40A8,8,0,0,1,224,40ZM80,208H48V176a8,8,0,0,0-16,0v40a8,8,0,0,0,8,8H80a8,8,0,0,0,0-16Zm136-40a8,8,0,0,0-8,8v32H176a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V176A8,8,0,0,0,216,168ZM40,88a8,8,0,0,0,8-8V48H80a8,8,0,0,0,0-16H40a8,8,0,0,0-8,8V80A8,8,0,0,0,40,88Zm128,96H88a16,16,0,0,1-16-16V88A16,16,0,0,1,88,72h80a16,16,0,0,1,16,16v80A16,16,0,0,1,168,184ZM88,168h80V88H88Z"></path>
+                  </svg>
+                </span>
+                <p className="text-gray-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <div className="flex max-w-xs space-x-2 px-4">
+                <span className="shrink-0 flex h-14 w-14 items-center justify-center rounded-xl bg-white p-2.5 text-rose-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28"
+                    height="28"
+                    fill="#f5384b"
+                    viewBox="0 0 256 256"
+                  >
+                    <path d="M164,80a28,28,0,1,0-28-28A28,28,0,0,0,164,80Zm0-40a12,12,0,1,1-12,12A12,12,0,0,1,164,40Zm36,96a40,40,0,1,0,40,40A40,40,0,0,0,200,136Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,200,200ZM56,136a40,40,0,1,0,40,40A40,40,0,0,0,56,136Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,56,200Zm136-80H152a8,8,0,0,1-5.66-2.34L120,91.31,99.31,112l34.35,34.34A8,8,0,0,1,136,152v48a8,8,0,0,1-16,0V155.31L82.34,117.66a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,0L155.31,104H192a8,8,0,0,1,0,16Z"></path>
+                  </svg>
+                </span>
+                <p className="text-gray-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="relative mt-20 hidden lg:block">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-auto my-6 h-10 w-10 animate-bounce rounded-full bg-blue-50 p-2 lg:hidden"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16 17l-4 4m0 0l-4-4m4 4V3"
+              ></path>
+            </svg>
+            <div
+              className="w-fit mx-auto flex overflow-hidden rounded-3xl bg-orange-400"
+              role="image-container"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,106 @@
 import React from "react";
 import Link from "next/link";
+import { useMemo } from "react";
+
+interface LIST_NAVBAR {
+  id: number;
+  label: string;
+  path: string;
+}
+
+const listsMenus: LIST_NAVBAR[] = [
+  {
+    id: 1,
+    label: "Home",
+    path: "/home",
+  },
+  {
+    id: 2,
+    label: "Shop",
+    path: "/shop",
+  },
+  {
+    id: 3,
+    label: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 4,
+    label: "Contact",
+    path: "/contact",
+  },
+  {
+    id: 5,
+    label: "About",
+    path: "/about",
+  },
+];
 
 export default function Header() {
   return (
-    <div className="flex justify-between p-10 bg-slate-200">
-      <h1>NextLearn</h1>
-      <ul className="flex justify-arround gap-2">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link href="/produk">Produk</Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="bg-white shadow">
+      <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
+        <div className="flex justify-between items-center">
+          <div>
+            <Link
+              className="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700"
+              href="/"
+            >
+              GoCommerce
+            </Link>
+          </div>
+
+          <div className="flex md:hidden">
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+              aria-label="toggle menu"
+            >
+              <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="md:flex items-center">
+          <div className="flex flex-col md:flex-row md:mx-6">
+            {listsMenus?.map((item: LIST_NAVBAR) => (
+              <Link
+                key={item.id}
+                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                href={item.path}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex justify-center md:block">
+            <a className="relative text-gray-700 hover:text-gray-600" href="#">
+              <svg
+                className="h-5 w-5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+
+              <span className="absolute top-0 left-0 rounded-full bg-indigo-500 text-white p-1 text-xs"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
